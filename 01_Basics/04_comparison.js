57afbe3dde1d0462fb71a65bba6f4da3abc3250b
Mohit-7777 <145227505+Mohit-7777@users.noreply.github.com>
2024-05-12 18:46:11 +0000

    comparison

---
+++ 01_Basics/04_comparison.js
@@ -0,0 +1,21 @@
+// console.log(2>1);
+// console.log(2>=1);
+// console.log(2<1);
+// console.log(2<=1);
+// console.log(2==1);
+// console.log(2!=1);
+
+// console.log("2" >1);
+// console.log("02" >1);
+
+// console.log(null>0);  // false null is converted into 0 here
+// console.log(null==0); // false null is not conveted into 0 here cuz It is not a comparison instead its a equality check
+// console.log(null>=0); // true null is converted into 0 here
+
+// console.log(undefined == 0); // false
+// console.log(undefined == 0); // false
+// console.log(undefined == 0); // false
+
+//== Strict check
+
+console.log("2" ===2);
