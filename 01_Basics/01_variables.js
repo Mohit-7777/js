40b4a46640bea10d0d46448b88c1795d2e2fea58
Mohit-7777 <145227505+Mohit-7777@users.noreply.github.com>
2024-05-12 18:45:37 +0000

    Variables

---
+++ 01_Basics/01_variables.js
@@ -0,0 +1,14 @@
+const accountId = 144553
+let accEmail = "abcd@google.com"
+var accPass = "12345"
+accountCity = "Jaipur"
+let accountState;
+
+// accountId = 2
+accEmail = "ab@mb.com"
+accPass = "54321"
+accountCity = "Dehradun"
+
+console.log(accountId);
+
+console.table([accountId, accEmail, accPass, accountCity, accountState]);
