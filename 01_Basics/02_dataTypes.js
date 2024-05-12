96188f53420155649fc3e73e7662e69d9edba1fc
Mohit-7777 <145227505+Mohit-7777@users.noreply.github.com>
2024-05-12 17:37:06 +0000

    datatypes

---
+++ 01_Basics/02_dataTypes.js
@@ -0,0 +1,24 @@
+"use strict"; // treat all JS as newer version
+
+// alert(3+3)
+
+//datatypes
+
+let name = "Mohit"
+let age = 22
+let isLoggedIn = false
+
+//number => 2 to the power 53
+//bigint 
+//string => ""
+//boolean => true/false
+//null => standalone value
+//undefined =>
+//symbol => unique
+
+//object 
+
+console.log(typeof age);
+
+console.log(typeof undefined); //undefined
+console.log(typeof null); //object
