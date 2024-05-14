6d677c063a019ebd4ef60e11fa3878f659636e8d
Mohit-7777 <145227505+Mohit-7777@users.noreply.github.com>
2024-05-14 16:28:34 +0000

    scopes

---
+++ 03_Basics/02_scopes.js
@@ -0,0 +1,48 @@
+// let a =10
+// const b=20
+// var c =30
+
+let a = 300
+
+if(true){
+    let a =10
+// const b=20
+// var c =30
+// console.log(a);
+}
+
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+
+function one(){
+    const username = "Mohit"
+    // console.log("mohittttt");
+
+    function two(){
+        const website = "Youtube"
+        // console.log(username);
+    }
+    // console.log(website);
+    two()
+}
+one()
+
+
+//+++++++++++++++++++++++++++++++++ ++++++++++++++++++++++++++++++++++++
+
+console.log(addone(5));             // Normal Function can be called by putting function call above the function
+function addone(num){               // Normal Function 
+    return num+1
+}
+// addone(5)
+
+
+
+// addTwo(5)                           //Can't access addTwo before initialization
+const addTwo = function(num){       //Expression or Variables
+    return num+2
+}
+
+
