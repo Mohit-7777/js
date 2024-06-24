fbff7c7e9d17e3a4beb7df78d3e16d295929994e
MoHit <145227505+Mohit-7777@users.noreply.github.com>
2024-06-24 16:33:23 +0000

    bigInt

--- 01_Basics/01_variables.js
+++ 01_Basics/01_variables.js
@@ -11,4 +11,12 @@ accountCity = "Dehradun"
 
 console.log(accountId);
 
+// BIG INT :- represented by n      range:- - (2^53-1) to + (2^53-1)
+
+let y = 9999999999999999;       // This will be rounded off to 10000000000000000
+
+let x = 999999999999999999999n; //BigInt... Here n is used for BigInt
+
+console.log(`${y} and ${x}`);
+
 console.table([accountId, accEmail, accPass, accountCity, accountState]);
